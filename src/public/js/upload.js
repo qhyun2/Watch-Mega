8fa0ace5b2b6ac3265303e12dae4663cbcb6ce16
Howard Yun <haoy2001@gmail.com>
2021-03-07 13:59:50 -0500

    Clean up unused files

--- src/public/js/upload.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-  $("#fileupload").fileupload({
-    url: "/uploads",
-    maxFileSize: 4 * 1000 * 1000 * 1000, // 4GB
-    maxChunkSize: 10 * 1000 * 1000, // 10 MB
-    acceptFileTypes: /(\.|\/)(mp4|avi|mov|mkv)$/i,
-  });
-});
