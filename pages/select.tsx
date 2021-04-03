d28d17173905274d970598d0cbd77514455293c1
Howard Yun <haoy2001@gmail.com>
2021-04-03 18:23:31 -0400

    Refactor Select page to functional component

--- pages/select.tsx
+++ pages/select.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useRouter, withRouter, NextRouter } from "next/router";
+import { useRouter } from "next/router";
 
 // authentication
 import { defaultAuth } from "../lib/Auth";
@@ -28,179 +28,49 @@ import {
 } from "@material-ui/core";
 import { Pagination, Skeleton } from "@material-ui/lab";
 
+const YoutubeSelector: React.FC = () => {
+  const [youtubeLink, setYoutubeLink] = useState("");
+  const router = useRouter();
+
+  return (
+    <Grid item container spacing={1} wrap="nowrap">
+      <Grid item style={{ flexGrow: 1 }}>
+        <TextField
+          id="outlined-basic"
+          label="Youtube Link"
+          fullWidth
+          value={youtubeLink}
+          onChange={(e) => setYoutubeLink(e.target.value)}
+        />
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ height: "100%" }}
+          onClick={() => {
+            if (!youtubeLink) return;
+            const regex = youtubeLink.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/);
+            const id = regex ? regex.pop() : null;
+            if (id) {
+              axios.post("/api/media/select", { src: "youtube:" + id }).then(() => {
+                router.push("/");
+              });
+            }
+          }}>
+          select
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
 interface HistoryItem {
   name: string;
   timestamp: number;
 }
 
-interface state {
-  historyLoading: boolean;
-  history: Map<number, HistoryItem>;
-  maxPages: number;
-  page: number;
-  textFieldYoutube: string;
-}
-
-interface props {
-  router: NextRouter;
-}
-
-class Select extends React.Component<props, state> {
-  loadedPages: Set<number>;
-
-  constructor(props) {
-    super(props);
-
-    this.loadedPages = new Set<number>();
-    this.state = {
-      historyLoading: true,
-      history: new Map<number, HistoryItem>(),
-      maxPages: 1,
-      page: 1,
-      textFieldYoutube: "",
-    };
-  }
-
-  loadHistory(page: number): Promise<void> {
-    if (this.loadedPages.has(page)) return;
-    this.setState({ historyLoading: true });
-    return axios
-      .get("/api/media/history", { params: { start: (page - 1) * 5, end: page * 5 - 1 } })
-      .then((response) => {
-        this.setState((state) => {
-          const newState = { ...state, historyLoading: false, maxPages: response.data.maxPages };
-          Array.from<HistoryItem>(response.data.history).forEach((item, index) =>
-            newState.history.set(page * 5 + index, item)
-          );
-          this.loadedPages.add(page);
-          return newState;
-        });
-      });
-  }
-
-  async componentDidMount(): Promise<void> {
-    await this.loadHistory(1);
-  }
-
-  renderHistoryItems(): JSX.Element[] {
-    if (this.state.historyLoading) {
-      return Array(5)
-        .fill(0)
-        .map((_, i) => (
-          <GridListTile key={i}>
-            <Skeleton variant="rect" height={108} animation="wave" />
-          </GridListTile>
-        ));
-    } else {
-      const index = this.state.page * 5;
-      const data: HistoryItem[] = [];
-      for (let i = index; i < index + 5; i++) {
-        data.push(this.state.history.get(i));
-      }
-
-      return data.map((item) =>
-        item ? (
-          <GridListTile key={item.name}>
-            <HistoryItem {...item}></HistoryItem>
-          </GridListTile>
-        ) : null
-      );
-    }
-  }
-
-  render(): JSX.Element {
-    return (
-      <React.Fragment>
-        <header>
-          <Navbar page="Select" />
-        </header>
-        <Box pt={4}>
-          <Container>
-            <Box pb={2}>
-              <Typography variant="h4" color="textPrimary">
-                History
-              </Typography>
-            </Box>
-            <Grid container spacing={8}>
-              <Grid item xs={8}>
-                {this.state.maxPages !== 0 ? (
-                  <>
-                    <GridList cellHeight={120} cols={1} style={{ minHeight: 640, alignContent: "flex-start" }}>
-                      {this.renderHistoryItems()}
-                    </GridList>
-                    <Box display="flex" justifyContent="center">
-                      <Pagination
-                        count={this.state.maxPages}
-                        page={this.state.page}
-                        onChange={(_, page) => {
-                          this.loadHistory(page);
-                          this.setState({ page });
-                          this.forceUpdate();
-                        }}
-                        color="primary"
-                      />
-                    </Box>
-                  </>
-                ) : (
-                  <Typography variant="h6" color="textSecondary">
-                    No history yet :/
-                  </Typography>
-                )}
-              </Grid>
-              <Grid item container xs={4} spacing={2} direction="column">
-                <Grid item>
-                  <Box>
-                    <Link href="/browse">
-                      <Button variant="contained" color="primary" size="large" fullWidth>
-                        Open file bowser
-                      </Button>
-                    </Link>
-                  </Box>
-                </Grid>
-                <Grid item>
-                  <Divider />
-                </Grid>
-                <Grid item container spacing={1}>
-                  <Grid item xs={9}>
-                    <TextField
-                      id="outlined-basic"
-                      label="Youtube Link"
-                      fullWidth
-                      value={this.state.textFieldYoutube}
-                      onChange={(e) => this.setState({ textFieldYoutube: e.target.value })}
-                    />
-                  </Grid>
-                  <Grid item xs={3}>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      style={{ height: "100%" }}
-                      onClick={() => {
-                        if (!this.state.textFieldYoutube) return;
-                        const regex = this.state.textFieldYoutube.match(
-                          /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
-                        );
-                        const id = regex ? regex.pop() : null;
-                        if (id) {
-                          axios.post("/api/media/select", { src: "youtube:" + id }).then(() => {
-                            this.props.router.push("/");
-                          });
-                        }
-                      }}>
-                      Select
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Container>
-        </Box>
-      </React.Fragment>
-    );
-  }
-}
-
-const HistoryItem = (props: { name: string; timestamp: number }) => {
+const HistoryItem: React.FC<HistoryItem> = (props) => {
   const [videoName, setVideoName] = useState(null);
   const router = useRouter();
   const url = props.name.split(":");
@@ -241,4 +111,142 @@ const HistoryItem = (props: { name: string; timestamp: number }) => {
   );
 };
 
-export default withRouter(Select);
+interface HistoryItemsProps {
+  isHistoryLoading: boolean;
+  page: number;
+  history: Map<number, HistoryItem>;
+}
+
+const HistoryItems: React.FC<HistoryItemsProps> = (props) => {
+  console.log(props);
+  if (props.isHistoryLoading) {
+    console.log("spooky scary skeletons");
+    return (
+      <React.Fragment>
+        {Array(5)
+          .fill(0)
+          .map((_, i) => (
+            <GridListTile key={i} style={{ width: "100%", paddingBottom: 10 }}>
+              <Skeleton variant="rect" height={108} animation="wave" />
+            </GridListTile>
+          ))}{" "}
+      </React.Fragment>
+    );
+  } else {
+    const index = props.page * 5;
+    const data: HistoryItem[] = [];
+    for (let i = index; i < index + 5; i++) {
+      data.push(props.history.get(i));
+    }
+
+    return (
+      <React.Fragment>
+        {data.map((item) =>
+          item ? (
+            <GridListTile key={item.name} style={{ width: "100%", paddingBottom: 10 }}>
+              <HistoryItem {...item}></HistoryItem>
+            </GridListTile>
+          ) : null
+        )}
+      </React.Fragment>
+    );
+  }
+};
+
+const HistoryPanel: React.FC = () => {
+  const [isHistoryLoading, setIsHistoryLoading] = useState(true);
+  const [history, setHistory] = useState(new Map<number, HistoryItem>());
+  const [maxPages, setMaxPages] = useState(1);
+  const [page, setPage] = useState(1);
+  const [loadedPages, setLoadedPages] = useState(new Set<number>());
+
+  useEffect(() => {
+    loadHistory(1);
+  });
+
+  function loadHistory(page: number): Promise<void> {
+    if (loadedPages.has(page)) return;
+    setIsHistoryLoading(true);
+    return axios
+      .get("/api/media/history", { params: { start: (page - 1) * 5, end: page * 5 - 1 } })
+      .then((response) => {
+        setHistory((history) => {
+          setIsHistoryLoading(false);
+          setMaxPages(response.data.maxPages);
+          Array.from<HistoryItem>(response.data.history).forEach((item, index) => history.set(page * 5 + index, item));
+          setLoadedPages((old) => old.add(page));
+          return history;
+        });
+      });
+  }
+
+  if (maxPages === 0) {
+    return (
+      <Typography variant="h6" color="textSecondary">
+        No history yet :/
+      </Typography>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <GridList cellHeight={120} cols={1} style={{ minHeight: 640, alignContent: "flex-start" }}>
+        <HistoryItems isHistoryLoading={isHistoryLoading} page={page} history={history} />
+      </GridList>
+      <Box display="flex" justifyContent="center">
+        <Pagination
+          count={maxPages}
+          page={page}
+          onChange={(_, page) => {
+            loadHistory(page);
+            setPage(page);
+          }}
+          color="primary"
+        />
+      </Box>
+    </React.Fragment>
+  );
+};
+
+const Select: React.FC = () => {
+  return (
+    <React.Fragment>
+      <header>
+        <Navbar page="Select" />
+      </header>
+      <Box pt={4}>
+        <Container>
+          <Box pb={2}>
+            <Typography variant="h4" color="textPrimary">
+              History
+            </Typography>
+          </Box>
+          <Grid container spacing={8}>
+            <Grid item xs={12} md={8}>
+              <HistoryPanel />
+            </Grid>
+            <Grid item container xs={12} md={4} spacing={2} direction="column">
+              <Grid item>
+                <Box>
+                  <Link href="/browse">
+                    <Button variant="contained" color="primary" size="large" fullWidth>
+                      Open file bowser
+                    </Button>
+                  </Link>
+                </Box>
+              </Grid>
+              <Grid item>
+                <Divider />
+              </Grid>
+              <Grid item>
+                <YoutubeSelector />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </React.Fragment>
+  );
+};
+
+export default Select;
