9c0805d0d07ae643a652dd58b36a35c96f725d45
Howard Yun <haoy2001@gmail.com>
2022-01-09 18:48:08 -0500

    Update Encoder to always encode and encode to mp4

--- src/Encoder.ts
+++ src/Encoder.ts
@@ -1,18 +1,14 @@
 import { logger } from "./Instances";
 import { promises as fsPromises } from "fs";
-import MediaInfoFactory from "mediainfo.js/dist/mediainfo";
-import type { MediaInfo, ReadChunkFunc, ResultObject } from "mediainfo.js/dist/types";
 import { exec } from "child_process";
 
 export async function encodeVideo(path: string): Promise<void> {
   const startTime = process.hrtime()[0];
 
   logger.info(`Encoding video: ${path}`);
-  const codecInfo = await getCodecInfo(path);
-  const videoCodec = "-c:v " + (codecInfo.video === "AVC" ? "copy" : "libx264");
-  const audioCodec = "-c:a " + (codecInfo.audio === "AAC" ? "copy" : "aac");
-  const settings = "-crf 22 -preset faster -f mp4";
-  const command = `ffmpeg -i "${path}" ${videoCodec} ${settings} ${audioCodec} "${path + ".mp4"}"`;
+  const tempFileName = path + ".temp.mp4";
+  const cmdLineFlags = "-c:v libx264 -c:a aac -crf 25 -preset veryfast -f mp4 -movflags +faststart -pix_fmt yuv420p";
+  const command = `ffmpeg -i "${path}" ${cmdLineFlags} "${tempFileName}"`;
   logger.info(`Encoding with command: ${command}`);
   await new Promise<void>((resolve, reject) => {
     exec(command, (error, stderr) => {
@@ -23,40 +19,6 @@ export async function encodeVideo(path: string): Promise<void> {
     });
   });
   await fsPromises.unlink(path);
-  await fsPromises.rename(path + ".mp4", path);
+  await fsPromises.rename(tempFileName, path.replace(/\.[^.]+$/, ".mp4"));
   logger.info(`Encoding finished in ${process.hrtime()[0] - startTime} second(s)`);
-}
-
-interface CodecInfo {
-  video: string;
-  audio: string;
-}
-
-export async function getCodecInfo(path: string): Promise<CodecInfo> {
-  let fileHandle: fsPromises.FileHandle | undefined;
-  let fileSize: number;
-  let mediainfo: MediaInfo | undefined;
-
-  const readChunk: ReadChunkFunc = async (size, offset) => {
-    const buffer = new Uint8Array(size);
-    await (fileHandle as fsPromises.FileHandle).read(buffer, 0, size, offset);
-    return buffer;
-  };
-
-  try {
-    fileHandle = await fsPromises.open(path, "r");
-    fileSize = (await fileHandle.stat()).size;
-    mediainfo = (await MediaInfoFactory()) as MediaInfo;
-    const result = <ResultObject>await mediainfo.analyzeData(() => fileSize, readChunk);
-    const tracks = result.media.track;
-    const video = tracks.find((x) => x["@type"] == "Video");
-    const audio = tracks.find((x) => x["@type"] == "Audio");
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return { video: video.Format, audio: audio.Format };
-  } finally {
-    fileHandle && (await fileHandle.close());
-    mediainfo && mediainfo.close();
-  }
 }
